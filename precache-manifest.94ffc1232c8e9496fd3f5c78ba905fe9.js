ee714b5d0368767943028914f4a41c684dd9f47e
aprillialee <aprilliapmlee@gmail.com>
2020-04-01 14:19:12 +0100

    Updates

---
+++ precache-manifest.94ffc1232c8e9496fd3f5c78ba905fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a25ada3b0a53517cd3fa04a86c47c7a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "ded3e3de26cf6f57cd63",
+    "url": "/monsters/static/css/main.e2400e48.chunk.css"
+  },
+  {
+    "revision": "4b626fc1e1b1be4a5d2c",
+    "url": "/monsters/static/js/2.76d92250.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.76d92250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded3e3de26cf6f57cd63",
+    "url": "/monsters/static/js/main.b2624975.chunk.js"
+  },
+  {
+    "revision": "5686043ec3762d983261",
+    "url": "/monsters/static/js/runtime-main.a05e8263.js"
+  }
+]);
